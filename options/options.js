c7318c772fe387d8442a8cf3510689e12962a13f
Crowdbotics <team@crowdbotics.com>
2024-04-29 07:59:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-waterfall-47730",
+  url: "https://fancy-waterfall-47730.botics.co",
+  api: "https://fancy-waterfall-47730.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
